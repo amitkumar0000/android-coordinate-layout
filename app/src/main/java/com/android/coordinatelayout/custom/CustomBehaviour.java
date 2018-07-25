51758d7bd1d3ee8e068bb3f5c0f1fce4904a8ad6
amit.kumar <amit.ap1924@gmail.com>
2018-07-26 01:02:33 +0530

    Custom Coordinate behaviour to move TextView on snackbar appearance

--- app/src/main/java/com/android/coordinatelayout/custom/CustomBehaviour.java
+++ app/src/main/java/com/android/coordinatelayout/custom/CustomBehaviour.java
@@ -4,13 +4,14 @@ import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class CustomBehaviour extends CoordinatorLayout.Behavior<TextView> {
+public class CustomBehaviour extends CoordinatorLayout.Behavior<View> {
     private final static String TAG = "CustomBehaviour";
 
     public CustomBehaviour(Context context, AttributeSet attrs) {
@@ -18,27 +19,14 @@ public class CustomBehaviour extends CoordinatorLayout.Behavior<TextView> {
     }
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, TextView child, View dependency) {
-        return dependency instanceof FloatingActionButton;
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return dependency instanceof Snackbar.SnackbarLayout;
     }
 
-    boolean flag = false;
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, TextView child, View dependency) {
-        int[] dependencyLocation = new int[2];
-        int[] childLocation = new int[2];
-
-        dependency.getLocationInWindow(dependencyLocation);
-        child.getLocationInWindow(childLocation);
-
-        float diff = childLocation[1] - dependencyLocation[1];
-        if(diff > 0 && flag) {
-            float scale = diff/(float)childLocation[1];
-            Log.d(TAG, "scale == " + scale);
-            child.setScaleX(1+scale+10);
-            child.setScaleY(1+scale+10);
-        }
-        flag = true;
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        float translationY = Math.min(0, ViewCompat.getTranslationY(dependency) - dependency.getHeight());
+        ViewCompat.setTranslationY(child, translationY);
         return false;
     }
 }
